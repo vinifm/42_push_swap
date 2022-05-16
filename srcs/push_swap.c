09fbfc7ec4f9f1f8e12bf9106d1b79bee8e13f45
Vinicius Ferreira de mendonca <vifm@outlook.com>
2022-05-16 15:24:28 -0300

    start error management

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,14 +6,35 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 16:41:09 by viferrei          #+#    #+#             */
-/*   Updated: 2022/05/12 13:04:02 by viferrei         ###   ########.fr       */
+/*   Updated: 2022/05/16 15:24:15 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	printf("%d\n", 12&23);
+	if (argc == 0)
+		return (1);
+
+	printf("hello");
+
+	if (check_ints)
+		return (1);
+
+	printf("world");
+
 	return(0);
+}
+
+int	check_ints(char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i++])
+	{
+		if (argv[i] > INT_MAX || argv[i] < INT_MIN)
+			return (1);
+	}
 }
